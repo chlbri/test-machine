9ea566ad572462f2184915060680edc28c8bc7b2
chlbri <chlbri.blac@gmail.com>
2022-01-14 03:01:28 +0000

    ok

--- wconfig/pack.ts
+++ wconfig/pack.ts
@@ -29,16 +29,10 @@ function main() {
   // #endregion
 
   // #region Config
-  const versions = (file.get('version') as string).split('.');
-  const versionN = Number.parseInt(versions[versions.length - 1]) + 1;
-  versions.pop();
-  versions.push(versionN + '');
   const typings = resetToMain(file.get('typings'));
   file.set('typings', typings);
   const _main = resetToMain(file.get('main'));
   file.set('main', _main);
-  const version = versions.join('.');
-  file.set('version', version);
   // #endregion
 
   file.save();
