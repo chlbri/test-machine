ca4610a822c67e168a240e2e5167c9139b9d7869
chlbri <chlbri.blac@gmail.com>
2022-01-14 12:38:04 +0000

    build and tsconfig

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { assign, createMachine } from 'xstate/lib';
 import { generateAsyncMachineTest, generateSyncMachineTest } from '../src';
 
