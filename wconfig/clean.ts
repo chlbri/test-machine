2fcf7664697682d8472e651d8d749ab92492bf9f
chlbri <chlbri.blac@gmail.com>
2022-01-14 12:51:28 +0000

    clean

--- wconfig/clean.ts
+++ wconfig/clean.ts
@@ -9,21 +9,8 @@ const CLEAN_COMMAND2 = 'git add . && git commit -am "clean"';
 const CLEAN_COMMAND35 = 'yarn git-push';
 const CLEAN_COMMAND4 = 'git checkout dev && git merge main';
 const CLEAN_COMMAND6 = 'git checkout dev';
-const PACKAGE = './package.json';
 
 function main() {
-  // #region Config
-  const file = editjson(PACKAGE);
-
-  const versions = (file.get('version') as string).split('.');
-  const versionN = Number.parseInt(versions[versions.length - 1]) + 1;
-  versions.pop();
-  versions.push(versionN + '');
-  const version = versions.join('.');
-  file.set('version', version);
-  // #endregion
-
-  file.save();
   exec(CLEAN_COMMAND2);
   exec(CLEAN_COMMAND35);
   exec(CLEAN_COMMAND4);
